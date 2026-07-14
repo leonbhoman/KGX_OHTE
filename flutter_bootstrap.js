f47327ac0e377a25b92f14e2e778fae899b52a95
leonbhoman <leonb.homan@gmail.com>
2026-07-14 12:55:25 +0200

    Built web
    Branch: main
    Commit: eccd92c084b86153ad00477ae811f425cd69afb0
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3527340136" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3857583534" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

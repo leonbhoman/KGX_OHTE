4ba64bb99384b0c8372e4d4b3769f045e24b00e2
leonbhoman <leonb.homan@gmail.com>
2026-07-16 14:56:11 +0200

    fine tuning the hover blocks v7

--- lib/main.dart
+++ lib/main.dart
@@ -108,13 +108,12 @@ List<Widget> _buildSwitchNodes() {
       {'left': 600.0, 'width': 150, 'top': 35.0, 'height': 40.0,   'label': 'Roads 53 to 59'}, // Blue
       {'left': 530.0, 'width': 150, 'top': 130.0, 'height': 50.0, 'label': 'Roads 46 to 52'}, // Red
       {'left': 520.0, 'width': 180, 'top': 190.0, 'height': 45.0,  'label': 'Roads 40 to 45'}, // Brown
-      {'left': 510.0, 'width': 150, 'top': 160.0, 'height': 60.0, 'label': 'Roads 32 to 39'}, // Green
-      {'left': 550.0, 'width': 150, 'top': 400.0, 'height': 105.0, 'label': 'Roads 24 to 31'}, // Red
+      {'left': 510.0, 'width': 180, 'top': 260.0, 'height': 60.0, 'label': 'Roads 32 to 39'}, // Green
+      {'left': 520.0, 'width': 180, 'top': 330.0, 'height': 60.0, 'label': 'Roads 24 to 31'}, // Red
       {'left': 550.0, 'width': 150, 'top': 500.0, 'height': 75.0,  'label': 'Roads 16 to 23'}, // Yellow
-      {'left': 550.0, 'width': 150, 'top': 600.0, 'height': 65.0,  'label': 'Roads 8 to 15'},  // Light Yellow block
-      {'left': 550.0, 'width': 150, 'top': 700.0, 'height': 85.0,  'label': 'Roads 1 to 7'},   // Green
-      {'left': 550.0, 'width': 150, 'top': 800.0, 'height': 90.0, 'label': 'Roads 1 to 7 (Lower)'}, // Red
-    ];
+      {'left': 550.0, 'width': 150, 'top': 600.0, 'height': 65.0,  'label': 'Roads 8 to 15'},  // Green
+      {'left': 550.0, 'width': 150, 'top': 700.0, 'height': 85.0,  'label': 'Roads 1 to 7'},   // Red
+      ];
 
     return zones.map((zone) {
       return Positioned(
